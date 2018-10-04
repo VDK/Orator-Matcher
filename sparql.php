e22ca6e3e6010c89a3098311baa1a637a2366bdd
Vera de Kok <veradekok@gmail.com>
2018-10-04 16:27:27 +0200

    Update sparql.php
    + strip_tags, error message for bad input

--- sparql.php
+++ sparql.php
@@ -18,22 +18,29 @@
 <form class="form-wrapper"  id="form"	>
 <a href="index.php">< back home</a>
   <div>
+<?php
+if (!isset($_GET['name']) || !is_array($_GET['name'])){
+	echo "<p style='color:red'>bad input</p></div></form>";
+}
+else{ ?>
         <input type="checkbox" id="sportsPersonCheck" name="feature"
                value="sportsPersonCheck" checked />
         <label for="sportsPersonCheck">include sportspeople</label>
     </div>
 </form>
 <div id='progressbar'></div>
-<ul id='names'>
-<?php
-
-$names = $_GET['name'];
-$names = array_unique($names);
-foreach ($names as $key => $name) {
-	$name = trim($name);
-	if ($name != ''){
-		echo "<li class='name' weight='-1'>".$name."<ul class='response' ></ul></li>";
+	<ul id='names'>";
+	<?php
+	$names = $_GET['name'];
+	$names = array_unique($names);
+	foreach ($names as $key => $name) {
+		$name = trim($name);
+		$name = strip_tags($name);
+		if ($name != ''){
+			echo "<li class='name' weight='-1'>".$name."<ul class='response' ></ul></li>";
+		}
 	}
+	echo "</ul>";
 }
 
 
